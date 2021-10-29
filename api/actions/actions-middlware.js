d1b32f2cf4a014584fa90710fc09b2dbcec2bd79
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-10-29 18:06:01 -0400

    endpoints complete

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,28 @@
-// add middlewares here related to actions
+const Action = require("./actions-model");
+
+async function validateId(req, res, next) {
+  Action.get(req.params.id)
+    .then((action) => {
+      if (action) {
+        req.action = action;
+        next();
+      } else {
+        next({ status: 404 });
+      }
+    })
+    .catch(next);
+}
+
+async function validateAction(req, res, next) {
+  const { project_id, description, notes } = req.body;
+  if (!project_id || !description || !notes) {
+    next({ status: 400 });
+  } else {
+    next();
+  }
+}
+
+module.exports = {
+  validateId,
+  validateAction,
+};
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,5 +1,7 @@
 const express = require("express");
-//pull middleware
+const { handleError } = require("../projects/projects-middleware");
+const { validateId, validateAction } = require("./actions-middlware");
+
 //eslint-disable-next-line
 const Action = require("./actions-model");
 
@@ -13,4 +15,42 @@ router.get("/", async (req, res) => {
     res.status(500);
   }
 });
+
+router.get("/:id", validateId, async (req, res, next) => {
+  try {
+    const action = await Action.get(req.params.id);
+    res.status(200).json(action);
+  } catch (er) {
+    next();
+  }
+});
+
+router.post("/", validateAction, async (req, res, next) => {
+  try {
+    const newAction = await Action.insert(req.body);
+    res.status(201).json(newAction);
+  } catch (er) {
+    next();
+  }
+});
+
+router.put("/:id", validateId, validateAction, async (req, res, next) => {
+  try {
+    const changes = await Action.update(req.params.id, req.body);
+    res.status(200).json(changes);
+  } catch (er) {
+    next();
+  }
+});
+
+router.delete("/:id", validateId, async (req, res, next) => {
+  try {
+    await Action.remove(req.params.id);
+    res.status(200).json({ message: "deleted" });
+  } catch (er) {
+    next();
+  }
+});
+
+router.use(handleError);
 module.exports = router;
