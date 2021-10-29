af3af7aa59466654efe41e7a1cd61c4c204ff058
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-10-29 12:28:35 -0400

    get basic from router

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,11 @@
-// Write your "projects" router here!
+const express = require("express");
+//pull middleware
+const Project = require("./projects-model");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send(`<div>success</div>`);
+});
+
+module.exports = router;
